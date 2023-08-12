290ddc0d529d385fe1ede1e465c5e270a1abfa66
Dion Dresschers <dion@dresschers.net>
2023-08-12 18:10:31 +0200

    Added templates

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask import render_template # now it can use templates
+
+# This file is `hello.py` but if it was `app.py`, than you don't have to specify `export FLASK_APP=app`.
+
+
+
+
+
+app = Flask(__name__) # We've just imported `Flask` from line 1. The `__name__`  is the app we currenty run (thus `hello.py`).
+
+# print(__name__) # This is the `hello` name, which is determined by the `hello.py` file.
+
+@app.route("/")
+def home(): 
+  return render_template('home.html') # This is the template file `home.html`, that Flask automatically will find because it is in the `templates` directory.
+#    return "Hello World..."
+
+#app.run(host="0.0.0.0", port=50000, debug=True) # Necessary to access WSL2 flask via Windows, in WSL pipenv run with 'python3 hello.py'
+
+@app.route("/about")
+def about(): # The route and the function name don't have to match.
+  return "This is a url shortner"
+
+
+
