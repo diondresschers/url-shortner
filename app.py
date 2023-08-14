868822303b4d39239958e132a8c147a4bb74f2da
Dion Dresschers <dion@dresschers.net>
2023-08-14 08:54:41 +0200

    POST requests

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template # now it can use templates
+from flask import request # This is for the GET-requests
 
 # This file is `hello.py` but if it was `app.py`, than you don't have to specify `export FLASK_APP=app`.
 
@@ -13,7 +14,7 @@ app = Flask(__name__) # We've just imported `Flask` from line 1. The `__name__` 
 
 @app.route("/")
 def home(): 
-  return render_template('home.html') # This is the template file `home.html`, that Flask automatically will find because it is in the `templates` directory.
+  return render_template('home.html', name="Dion") # This is the template file `home.html`, that Flask automatically will find because it is in the `templates` directory.
 #    return "Hello World..."
 
 #app.run(host="0.0.0.0", port=50000, debug=True) # Necessary to access WSL2 flask via Windows, in WSL pipenv run with 'python3 hello.py'
@@ -21,6 +22,11 @@ def home():
 @app.route("/about")
 def about(): # The route and the function name don't have to match.
   return "This is a url shortner"
+
+@app.route("/your-url")
+def your_url(): # Python functions, don't allow dashes, so we use `_`.
+  return render_template("your-url.html", code=request.args['code']) # The latter is to access the GET-request for the field `code` that we put into an HTML form.
+
 
 
 
